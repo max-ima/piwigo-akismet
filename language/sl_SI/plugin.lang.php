5fd2b027ae8b2c82a8d6027876c15db158dfbafa
max-ima <96241406+max-ima@users.noreply.github.com>
2022-01-06 10:14:39 +0100

    sl_SI language

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,26 @@
+<?php
+
+//admin.tpl
+$lang['This plugin verifies if your comments are spam using the <a href="http://www.akismet.com" target="_blank">akismet online service</a>.'] = 'Ta vtičnik preveri, ali so vaši komentarji vsiljeni z <a href="http://www.akismet.com" target="_blank">spletno storitvijo Akismet</a>.';
+$lang['Akismet has caught'] = 'Akismet je ujel';
+$lang['spam comments'] = 'vsiljenih komentarjev';
+$lang['out of'] = 'od';
+$lang['comments'] = 'komentarjev';
+$lang['reset counters'] = '<b>Ponastavi števce</b>';
+$lang['More statistics on'] = 'Več statistike na';
+$lang['akismet site'] = 'Akismet strani';
+$lang['Akismet configuration'] = 'Akismet konfiguracija';
+$lang['Akismet API Key:'] = 'Akismet API ključ:';
+$lang['Signup for a personal usage key here:'] = 'Prijavite se za ključ za osebno uporabo tukaj:';
+$lang['Action when spam is detected:'] = 'Ukrep, ko je zaznana vsiljenost';
+$lang['moderate'] = 'zmerno';
+$lang['reject'] = 'zavrniti';
+$lang['Test your configuration'] = 'Testirajte svojo konfiguracijo';
+
+
+//maintain.inc.php
+$lang['You need to define the Akismet api key'] = 'Definirati morate Akismet API ključ';
+
+//akismet.class.php
+$lang['AKISMET_INVALID_KEY'] = 'Vaš API Akismet ključ ni veljaven.';
+?>
